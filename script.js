633b12482f448c3ed9a15ba539fad43085c55c09
meflamey <somarghyakarmakar@gmail.com>
2026-07-11 11:39:48 +0530

    Rework

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const copyBtn = document.getElementById("copyBtn");
 
 function copy(){
     navigator.clipboard.writeText(`meflamey.dev`);
-    copyBtn.textContent = `Copied!`;
-    setTimeout(() => {
-        copyBtn.textContent = `Copy`;
-    }, 1500);
 }
 
 copyBtn.onclick = copy
